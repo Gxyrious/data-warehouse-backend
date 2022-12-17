8b7eb6e33ffc8328520dee2278043977d5443faf
ChillyGWind <1061633882@qq.com>
2022-12-17 22:38:35 +0800

    Update comprehensive.py

--- neo4j_/api/comprehensive.py
+++ neo4j_/api/comprehensive.py
@@ -19,58 +19,73 @@ def neo4j_comprehensive_movie():
     times = conditions.get('times')
     inname = conditions.get('name')
     page = conditions.get('page')
-    per_page =  conditions.get('per_page')
+    per_page = conditions.get('per_page')
     result = {'data': {}}
+    end = page * per_page
     # calculate time
     start_time = time.time()
     if a == 'actor' and b == 'actor':
         node_matcher = NodeMatcher(graph)
         relationship_matcher = RelationshipMatcher(graph)
         node1 = node_matcher.match('actor').where(name=inname).first()
-        filnum = "_.times>=" + str(times)
-        relationship = list(relationship_matcher.match([node1], r_type='actope').where(filnum).all())
+        filwhere = "(_.name='" + str(inname) + "' OR " + "_.name1='" + str(inname) + "') AND " + "_.times>=" + str(
+            times)
+        relationship = list(relationship_matcher.match(None, r_type='actope').where(filwhere).all())
         print(relationship)
         for i in range(len(relationship)):
             x = relationship[i].get("title").split(',')
-            resulted.append({"name": relationship[i].get("name"), "title": x, "times": relationship[i].get("times")})
+            if relationship[i].get("name") != inname:
+                resulted.append(
+                    {"name": relationship[i].get("name"), "title": x, "times": relationship[i].get("times")})
+            else:
+                resulted.append(
+                    {"name": relationship[i].get("name1"), "title": x, "times": relationship[i].get("times")})
         result.update({"count": len(relationship)})
-        end = page * per_page
         if end > len(resulted):
             end = len(resulted)
         result.update({'data': resulted[(page - 1) * per_page:end]})
+        end_time = time.time()
+        consuming_time = end_time - start_time
+        result.update({'consuming_time': consuming_time})
+        return jsonify(result)
         # ..
     elif a == 'director' and b == 'actor':
         node_matcher = NodeMatcher(graph)
         relationship_matcher = RelationshipMatcher(graph)
         node1 = node_matcher.match('director').where(name=inname).first()
         filnum = "_.times>=" + str(times)
-        relationship = list(relationship_matcher.match([node1], r_type='directope').where(filnum).all())
+        relationship = list(relationship_matcher.match(None, r_type='directope', dname=inname).where(filnum).all())
         print(relationship)
         for i in range(len(relationship)):
             x = relationship[i].get("title").split(',')
-            resulted.append({"name": relationship[i].get("name"), "title": x, "times": relationship[i].get("times")})
+            resulted.append({"name": relationship[i].get("aname"), "title": x, "times": relationship[i].get("times")})
         result.update({"count": len(relationship)})
-        end = page * per_page
         if end > len(resulted):
             end = len(resulted)
         result.update({'data': resulted[(page - 1) * per_page:end]})
+        end_time = time.time()
+        consuming_time = end_time - start_time
+        result.update({'consuming_time': consuming_time})
+        return jsonify(result)
         # ..
     elif a == 'actor' and b == 'director':
         node_matcher = NodeMatcher(graph)
         relationship_matcher = RelationshipMatcher(graph)
         node1 = node_matcher.match('actor').where(name=inname).first()
         filnum = "_.times>=" + str(times)
-        relationship = list(relationship_matcher.match([node1], r_type='directope').where(filnum).all())
+        relationship = list(relationship_matcher.match(None, r_type='directope', aname=inname).where(filnum).all())
         print(relationship)
         for i in range(len(relationship)):
             x = relationship[i].get("title").split(',')
-            resulted.append({"name": relationship[i].get("name"), "title": x, "times": relationship[i].get("times")})
+            resulted.append({"name": relationship[i].get("dname"), "title": x, "times": relationship[i].get("times")})
         result.update({"count": len(relationship)})
-        end = page * per_page
         if end > len(resulted):
             end = len(resulted)
         result.update({'data': resulted[(page - 1) * per_page:end]})
-
+        end_time = time.time()
+        consuming_time = end_time - start_time
+        result.update({'consuming_time': consuming_time})
+        return jsonify(result)
         # ..
     end_time = time.time()
     consuming_time = end_time - start_time
